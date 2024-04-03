8d83be4d9d563be0f6db9c418403492cee9eb57f
kaVII-x <ka52jiguli@yandex.ru>
2024-04-03 15:47:42 +0400

    version 1.1

---
+++ wallet_app/wallet_app/src/test/java/com/yanfeofanov/wallet_app/WalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanfeofanov.wallet_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
